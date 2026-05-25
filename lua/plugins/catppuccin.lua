c3b25e4e09756128c6a2fbc564d5ea8faabc377d
BalajiLeninrajan <balaji@finta.com>
2026-05-25 14:55:30 -0700

    pretty glass stuff

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -5,4 +5,12 @@ return {
       colorscheme = "catppuccin-mocha",
     },
   },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      -- Let Ghostty's background-opacity (0.75) and blur show through
+      transparent_background = true,
+    },
+  },
 }
