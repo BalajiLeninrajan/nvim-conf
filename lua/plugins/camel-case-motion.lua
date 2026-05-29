05a3bbe0a007eb687dc64ad759739f0c531646e4
BalajiLeninrajan <balaji@finta.com>
2026-05-28 19:42:22 -0700

    maor plugs

---
+++ lua/plugins/camel-case-motion.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "bkad/CamelCaseMotion",
+    keys = {
+      { "w", "<Plug>CamelCaseMotion_w", mode = { "n", "x", "o" }, desc = "CamelCase Forward" },
+      { "b", "<Plug>CamelCaseMotion_b", mode = { "n", "x", "o" }, desc = "CamelCase Backward" },
+      { "e", "<Plug>CamelCaseMotion_e", mode = { "n", "x", "o" }, desc = "CamelCase End" },
+      { "ge", "<Plug>CamelCaseMotion_ge", mode = { "n", "x", "o" }, desc = "CamelCase Previous End" },
+    },
+  },
+}
---
+++ lua/plugins/copy-reference.lua
@@ -0,0 +1,7 @@
+return {
+  "cajames/copy-reference.nvim",
+  opts = {},
+  keys = {
+    { "<leader>y", "<cmd>CopyReference line<cr>", mode = { "n", "v" }, desc = "Copy file:line reference" },
+  },
+}
